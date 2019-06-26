05eccc76bde97966671c6b2c4b5cfc44d52b1384
Vladimir Bozhenov <ve.bozhenov@gmail.com>
2019-06-26 09:09:42 +0300

    no message

--- CurrencyExchange/ViewController/MainViewController.m
+++ CurrencyExchange/ViewController/MainViewController.m
@@ -130,7 +130,7 @@
                                                                                  250,
                                                                                  [self.view bounds].size.width - 200,
                                                                                  50)];
-//        self.inputValueTextField.delegate = self;
+        self.inputValueTextField.delegate = self;
         [self.inputValueTextField setBackgroundColor:[UIColor whiteColor]];
         [self.inputValueTextField setBorderStyle:(UITextBorderStyleLine)];
         [self.inputValueTextField setPlaceholder:@"Enter value"];
