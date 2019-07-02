4850199dfe25aad70739e63a1f7a95725745a6f2
Vladimir Bozhenov <ve.bozhenov@gmail.com>
2019-07-02 13:15:00 +0300

    no message

---
+++ CurrencyExchange/Cell/CurrencyTableViewCell 2.m
@@ -0,0 +1,68 @@
+//
+//  CurrencyTableViewCell.m
+//  CurrencyExchange
+//
+//  Created by Vladimir Bozhenov on 25/06/2019.
+//  Copyright © 2019 Vladimir Bozhenov. All rights reserved.
+//
+
+#import "CurrencyTableViewCell.h"
+
+@interface CurrencyTableViewCell ()
+
+@property (nonatomic, strong) UILabel *currencyName;
+@property (nonatomic, strong) UILabel *currencyValue;
+
+@end
+
+@implementation CurrencyTableViewCell
+
+- (void)awakeFromNib {
+    [super awakeFromNib];
+}
+
+- (instancetype)initWithStyle:(UITableViewCellStyle)style reuseIdentifier:(NSString *)reuseIdentifier {
+    self = [super initWithStyle:style reuseIdentifier:reuseIdentifier];
+    
+    if (self) {
+        
+        self.currencyName = [[UILabel alloc] initWithFrame:CGRectMake(8,
+                                                                      20,
+                                                                      300,
+                                                                      20)];
+        [self.currencyName setTextAlignment:NSTextAlignmentLeft];
+        [self.currencyName setFont:[UIFont systemFontOfSize:15
+                                                     weight:UIFontWeightThin]];
+        [self.contentView addSubview:self.currencyName];
+        
+        self.currencyValue = [[UILabel alloc] initWithFrame:CGRectMake(self.bounds.size.width - 80,
+                                                                       20,
+                                                                       150,
+                                                                       20)];
+        [self.currencyValue setTextAlignment:NSTextAlignmentRight];
+        [self.currencyValue setFont:[UIFont systemFontOfSize:15
+                                                      weight:UIFontWeightBold]];
+        [self.contentView addSubview:self.currencyValue];
+    }
+    
+    [self setSelectionStyle:UITableViewCellSelectionStyleNone];
+
+    return self;
+}
+
+- (void)prepareForReuse {
+    [super prepareForReuse];
+    self.currencyName.text = nil;
+    self.currencyValue.text = nil;
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+}
+
+- (void)setupCellWithCurrency:(Currency*)currency {
+    self.currencyName.text = [NSString stringWithFormat:@"%@ %@", currency.nominal, currency.name];
+    self.currencyValue.text = [NSString stringWithFormat:@"%.4f", [currency.value floatValue]];
+}
+
+@end
---
+++ CurrencyExchange/ViewController/StartViewController 2.m
@@ -0,0 +1,76 @@
+//
+//  StartViewController.m
+//  CurrencyExchange
+//
+//  Created by Vladimir Bozhenov on 21/06/2019.
+//  Copyright © 2019 Vladimir Bozhenov. All rights reserved.
+//
+
+#import "StartViewController.h"
+#import "MainViewController.h"
+
+@interface StartViewController ()
+
+@end
+
+@implementation StartViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    [self.view setBackgroundColor:[UIColor lightGrayColor]];
+    
+    //Label
+    UILabel *label = [[UILabel alloc] initWithFrame:CGRectMake(0,
+                                                               150,
+                                                               [self.view bounds].size.width,
+                                                               50)];
+    [label setBackgroundColor:[UIColor lightGrayColor]];
+    [label setTextColor:[UIColor blueColor]];
+    [label setText:@"CURRENCY EXCHANGE"];
+    [label setTextAlignment:(NSTextAlignmentCenter)];
+    [label setFont:[UIFont systemFontOfSize:30
+                                     weight:(UIFontWeightBold)]];
+    [self.view addSubview:label];
+    
+    //ImageView
+    UIImageView *logo = [[UIImageView alloc] initWithFrame:CGRectMake(0,
+                                                                      300,
+                                                                      [self.view bounds].size.width,
+                                                                      100)];
+    [logo setImage:[UIImage imageNamed:@"konverter"]];
+    [logo setBackgroundColor:[UIColor lightGrayColor]];
+    [logo setContentMode:UIViewContentModeScaleAspectFill];
+    [self.view addSubview:logo];
+    
+    //Button
+    UIButton *startButton = [[UIButton alloc] initWithFrame:CGRectMake([self.view bounds].size.width / 2 - 100,
+                                                                       500,
+                                                                       200,
+                                                                       30)];
+    [startButton setTitle:@"Push to start"
+                 forState:UIControlStateNormal];
+    [startButton setTitleColor:[UIColor blueColor]
+                      forState:UIControlStateNormal];
+    [startButton addTarget:self
+                    action:@selector(openMainViewController)
+          forControlEvents:(UIControlEventTouchUpInside)];
+    [self.view addSubview:startButton];
+    
+    //ActivityIndicator
+    UIActivityIndicatorView *activityIndicator = [[UIActivityIndicatorView alloc]
+                                                  initWithFrame:CGRectMake([self.view bounds].size.width / 2 - 25,
+                                                                           [self.view bounds].size.height - 200,
+                                                                           50,
+                                                                           50)];
+    [activityIndicator setActivityIndicatorViewStyle:(UIActivityIndicatorViewStyleWhiteLarge)];
+    [activityIndicator setHidesWhenStopped:true];
+    [activityIndicator startAnimating];
+    [self.view addSubview:activityIndicator];
+}
+
+-(void) openMainViewController {
+    MainViewController *mainViewController = [[MainViewController alloc] init];
+    [self.navigationController pushViewController:mainViewController animated:true];
+};
+
+@end
